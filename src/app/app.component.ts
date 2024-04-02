189059dd604ae3f2e929009e084a3b84a0f1098d
Geoff Munn <geoff@wxoutside.tools>
2024-04-02 21:55:04 +1300

    Demo mostly working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HomeComponent,
+    RouterOutlet,
+    RouterModule
+  ],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styles: [],
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+//bootstrapApplication(AppComponent, appConfig)
+// .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      //provideProtractorTestingSupport(),
+      provideRouter(routes)
+    ]
+  }
+).catch(err => console.error(err));
