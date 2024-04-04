100e8fbac56c5f082c29e97767caa05b50cf1e5c
Geoff Munn <geoff@wxoutside.tools>
2024-04-04 15:48:58 +1300

    Extra steps added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+
+declare function getToday(): any;
+declare function greetings(name: any): any;
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './game.component.html', 
   styleUrl: './game.component.css'
 })
 
-export class GameComponent {
+export class GameComponent implements OnInit{
 
+  showHide = {
+    'intro': true,
+    'howToPlay': false,
+    'theMap': false,
+    'scenarios': false,
+    'pickASide': false,
+  }
+  
+  ngOnInit() {
+    //getToday(); // without param
+    //greetings('geoff'); // with param
+
+    //this.showHide['intro'] = true
+    //this.showHide['theMap'] = true
+  }
+
+  nextPage(nextDiv:string){
+    for (let key in this.showHide) {
+      //let value = this.showHide[key as keyof typeof this.showHide]
+      
+      if (key == nextDiv) {
+        this.showHide[key as keyof typeof this.showHide] = true
+      } else {
+        this.showHide[key as keyof typeof this.showHide] = false
+      }
+    }
+  }
 }
