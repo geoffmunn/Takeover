7984c7a2a9f03148068f0211b85da6e1dd897223
Geoff Munn <geoff@wxoutside.tools>
2024-04-08 20:42:59 +1200

    New services created

---
+++ src/app/game/services/govt-types.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GovtTypesService {
+
+  govtTypes: Map<string, number>
+    
+  constructor() {
+    this.govtTypes = new Map<string, number>();
+
+    this.govtTypes.set('Communist', 1); 
+    this.govtTypes.set('Socialist', 2);
+    this.govtTypes.set('Liberal', 3);
+    this.govtTypes.set('Rightwing', 4);
+    this.govtTypes.set('Fascist', 5);
+  }
+}
