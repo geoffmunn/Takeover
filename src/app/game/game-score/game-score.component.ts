820b3eca41255977733a6e5206a6d31001b5a4c2
Geoff Munn <geoff@wxoutside.tools>
2024-04-22 23:01:02 +1200

    old code removed

--- src/app/game/game-score/game-score.component.ts
+++ src/app/game/game-score/game-score.component.ts
@@ -12,25 +12,4 @@ export class GameScoreComponent {
   @Input() userScore: number = 0
   @Input() govtScore: number = 0
 
-
-  // ngAfterViewInit(): void {
-  //   // console.log(this.user)
-  //   // console.log (this.test)
-
-  //   // this.userPlayer = this.user
-  //   // this.govtPlayer = this.user
-
-  //   this.geoff = this.test
-
-    
-  //   console.log (this.geoff)
-  //   this.userScore = this.geoff.popularity
-
-  // }
-  
-  // ngOnChanges() {
-   
-  //   this.userScore = this.geoff.popularity
-    
-  // }
 }
