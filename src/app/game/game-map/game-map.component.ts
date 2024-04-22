ed2699ff2a73abb124f3bfd449d5c0bfbc3b1b76
Geoff Munn <geoff@wxoutside.tools>
2024-04-22 23:01:18 +1200

    mood now calculated

--- src/app/game/game-map/game-map.component.ts
+++ src/app/game/game-map/game-map.component.ts
@@ -1,5 +1,7 @@
-import { Component, AfterViewInit, ElementRef, Renderer2, Output, EventEmitter} from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Input, Renderer2, Output, EventEmitter} from '@angular/core';
 import { BuildingsService } from '../services/buildings.service';
+import { BuildingService } from '../services/building.service';
+import { PlayerService } from '../services/player.service';
 
 @Component({
   selector: 'app-game-map',
@@ -10,16 +12,67 @@ import { BuildingsService } from '../services/buildings.service';
 })
 export class GameMapComponent implements AfterViewInit  {
 
-  @Output() valueChange = new EventEmitter();
+  @Input() user!: PlayerService
+  @Input() govt!: PlayerService
+
+  @Output() movesChange = new EventEmitter();
   counter = 0;
 
   mapBuildings: any
+  grid = new Array()
+  rebel_ownership: number  = -1
+  neutral_ownership: number = 0
+  govt_ownership: number = 1
 
-  valueChanged() { // You can give any function name
+  updateRemainingMoves() { // You can give any function name
 
     this.counter = this.counter + 1;
-    this.valueChange.emit(this.counter);
+    this.movesChange.emit(this.counter);
   }
+
+  calculateMood(x: number, y: number){				
+		var squareMood: number = 0
+    
+    if (y - 1 >= 0){
+      console.log (this.grid);
+      console.log (y-1);
+
+      squareMood += this.grid[(y-1)][x]['owner']
+      if (x - 1 >= 0){
+        squareMood += this.grid[(y - 1)][x - 1]['owner']
+      }
+
+      if (x + 1 < this.grid[(y - 1)].length){
+        squareMood += this.grid[(y - 1)][(x + 1)]['owner']
+      }
+    }
+    if (y + 1 < this.grid.length){
+      squareMood +=  this.grid[y + 1][x]['owner']
+      if (x - 1 >= 0)
+        squareMood += this.grid[y + 1][x - 1]['owner']
+      
+      if (x + 1 < this.grid[y + 1].length)
+        squareMood +=  this.grid[y + 1][x + 1]['owner']
+    }
+
+    if ((x-1) >= 0){
+      squareMood += this.grid[y][(x-1)]['owner']
+    }
+
+    if (x + 1 < this.grid[y].length){
+      squareMood += this.grid[y][x+1]['owner']
+    }
+
+    
+		// var b=this.grid[(y-1)][x]['owner'] + this.grid[(y+1)][x]['owner'] + this.grid[y][(x+1)]['owner'] + this.grid[y][(x-1)]['owner'];
+
+		// b=b+this.grid[(y-1)][(x-1)]['owner'] + this.grid[(y+1)][(x+1)]['owner'] + this.grid[(y-1)][(x+1)]['owner']+ this.grid[(y+1)][(x-1)]['owner'];
+			
+		//return b;
+    console.log ('square mood:', squareMood)
+    
+    return squareMood;
+	}
 
   activateBuilding($event: any){
     console.log($event.target.parentElement.parentElement.parentElement)
@@ -31,7 +84,49 @@ export class GameMapComponent implements AfterViewInit  {
 
     console.log(curEl.getAttribute('data-building-name'))
     
-    this.valueChanged()
+    var building:BuildingService = this.grid[curEl.getAttribute('data-row')][curEl.getAttribute('data-col')]['building']
+    
+    console.log (building)
+
+    console.log ('data-row:', curEl.getAttribute('data-row'))
+    console.log ('data-col:', curEl.getAttribute('data-col'))
+
+    var mood = this.calculateMood(parseInt(curEl.getAttribute('data-col')), parseInt(curEl.getAttribute('data-row')))
+
+    // console.log (this.user)
+    // console.log (this.govt)
+    // console.log ('---')
+    // console.log (this.user.popularity)
+    // console.log (this.govt.popularity)
+    // console.log ('---')
+    // b=buildings[building_num]['loyalty']+b/2+govt_popularity-rebel_popularity;
+    var decision = building.liklihood +(mood/2) + this.govt.popularity - this.user.popularity;
+
+    if (decision < 0)
+      decision = 0
+    if (decision > 5)
+      decision = 5
+    
+    console.log ('decision:', decision)
+		// if(b<1)
+		// 	b=1;
+		// if(b>5)
+		// 	b=5;
+
+
+    this.updateRemainingMoves()
+  }
+
+  activateStreet($event: any){
+    console.log($event.target)
+
+    //console.log($event.target.getAttribute('data-row'), $event.target.getAttribute('data-col'))
+
+    var mood = this.calculateMood(parseInt($event.target.getAttribute('data-col')), parseInt($event.target.getAttribute('data-row')))
+
+
+    this.updateRemainingMoves()
+
   }
 
   ngAfterViewInit(): void {
@@ -88,7 +183,24 @@ export class GameMapComponent implements AfterViewInit  {
       count += 1
 
     })
+
+    // Set up the grid object. This is what we use to calculate moves and ownerships
+    count = 0;
+    for(var y=0; y<7; y++){
+			this.grid[y] = new Array();
+			for(var x = 0; x < 11; x ++){
+			  this.grid[y][x] = new Array();
+				this.grid[y][x]['owner'] = this.neutral_ownership;
+        if (y % 2 == 1 && x % 2 == 1){
+					this.grid[y][x]['type'] = 'building';
+          this.grid[y][x]['building'] = this.buildings.buildings[randomNums[count]]
+          count += 1;
+        } else
+					this.grid[y][x]['type'] = 'square';
+			}
+		}
     
+    console.log (this.grid)
 	}
 
   constructor(public buildings: BuildingsService, private el: ElementRef, private renderer: Renderer2) {
