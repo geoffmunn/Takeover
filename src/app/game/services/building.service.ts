bbe6f43134afd8241dfc8c5de4137c1ff349b7dd
Geoff Munn <geoff@wxoutside.tools>
2024-05-27 10:32:55 +1200

    Functions documented

--- src/app/game/services/building.service.ts
+++ src/app/game/services/building.service.ts
@@ -28,7 +28,17 @@ export class BuildingService {
     
   }
 
-  setInitialValues(userType: number, userPopularity: number, govtType: number, govtPopularity: number){
+  /**
+   * This is a special function, called at the start when the buildings are initalised.
+   * 
+   * @param userType 
+   * @param userPopularity 
+   * @param govtType 
+   * @param govtPopularity 
+   * 
+   * @returns void
+   */
+  setInitialValues(userType: number, userPopularity: number, govtType: number, govtPopularity: number): void{
     let score: number     = 3 - Math.abs(this.leaning - govtType) + Math.abs(this.leaning - userType) + this.proGovernment
     let liklihood: number = Math.floor(score + govtPopularity - userPopularity)
 
@@ -42,9 +52,19 @@ export class BuildingService {
     this.mood      = this.getMoodFromLiklihood(liklihood)
   }
 
-  calculateLiklihood(userPopularity: number, govtPopularity: number, squareMood: number){
+  /**
+   * Based on the provided mood and popularities,
+   * calculate the liklihood that this building or street will switch.
+   * 
+   * @param userPopularity 
+   * @param govtPopularity 
+   * @param squareMood 
+   * 
+   * @returns number
+   */
+  calculateLiklihood(userPopularity: number, govtPopularity: number, squareMood: number): number{
 
-    var liklihood = Math.floor(this.liklihood + (squareMood / 2) + govtPopularity - userPopularity);
+    var liklihood:number = Math.floor(this.liklihood + (squareMood / 2) + govtPopularity - userPopularity);
 
     if (liklihood > 5)
       liklihood = 5
@@ -56,11 +76,27 @@ export class BuildingService {
     return liklihood
   }
 
-  setGridCoords(x:number, y:number){
+  /**
+   * Store the provided coordinates as the grid location for this building.
+   * This is called when the grid is initially set up at the start of the game.
+   * 
+   * @param x 
+   * @param y 
+   * 
+   * @returns void
+   */
+  setGridCoords(x:number, y:number): void{
     this.grid = {'x': x, 'y': y};
   }
 
-  getMoodFromLiklihood(liklihood: number) {
+  /**
+   * Based on the liklihood number, convert this into a readable string
+   * 
+   * @param liklihood 
+   * 
+   * @returns string
+   */
+  getMoodFromLiklihood(liklihood: number): string {
 
     var moods = Array();
 
