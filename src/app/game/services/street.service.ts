d41d18fe1c939bd105fb4fddd2aaf4b019894984
Geoff Munn <geoff@wxoutside.tools>
2024-06-01 17:24:07 +1200

    Street HTML element now stored

--- src/app/game/services/street.service.ts
+++ src/app/game/services/street.service.ts
@@ -5,23 +5,16 @@ import { Inject, Injectable } from '@angular/core';
 })
 export class StreetService {
 
-  public grid: any            = {};   // A json object of the grid coordinates
+  public element!: HTMLElement
+  public grid: any             = {};   // A json object of the grid coordinates
   public image: string         = '';  // Static - doesn't change
-  //public leaning: number       = 0;   // Only used at the starting summary
-  //public liklihood: number     = 0;   // This is set at the beginning and is the reference for all takeover attempts
   public mood: string          = '';  // A string representation of floating liklihood value
-  //public name: string          = '';  // Static - doesn't change
   public owner: number         = 0    // Changes depending on the game progress
-  //public points: number        = 0;   // Static - doesn't change
-  //public proGovernment: number = 0;   // Only used at the starting summary
-
-  constructor(@Inject(Number) x: number, @Inject(Number) y: number) {
-    this.grid          = {'x': x, 'y': y}
-    this.image         = 'road' + y + '_' + x + '.png';
-    //this.leaning       = leaning;
-    //this.name          = name;
-    this.owner         = 0
-    //this.points        = points
-    //this.proGovernment = proGovernment;
+  
+  constructor(@Inject(Number) x: number, @Inject(Number) y: number, element: HTMLElement) {
+    this.element = element
+    this.grid    = {'x': x, 'y': y}
+    this.image   = 'road' + y + '_' + x + '.png';
+    this.owner   = 0
   }
 }
