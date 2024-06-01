46c537c608caf7134b572fa0164574711a08aade
Geoff Munn <geoff@wxoutside.tools>
2024-06-01 17:24:39 +1200

    building action and consequence now part of building object

--- src/app/game/services/building.service.ts
+++ src/app/game/services/building.service.ts
@@ -4,15 +4,17 @@ import { LowerCasePipe } from '@angular/common';
 
 export class BuildingService {
 
-  public grid: any            = {};   // A json object of the grid coordinates
-  public image: string         = '';  // Static - doesn't change
-  public leaning: number       = 0;   // Only used at the starting summary
-  public liklihood: number     = 0;   // This is set at the beginning and is the reference for all takeover attempts
-  public mood: string          = '';  // A string representation of floating liklihood value
-  public name: string          = '';  // Static - doesn't change
-  public owner: number         = 0    // Changes depending on the game progress
-  public points: number        = 0;   // Static - doesn't change
-  public proGovernment: number = 0;   // Only used at the starting summary
+  public action: string          = '';
+  public grid: any               = {};  // A json object of the grid coordinates
+  public image: string           = '';  // Static - doesn't change
+  public leaning: number         = 0;   // Only used at the starting summary
+  public liklihood: number       = 0;   // This is set at the beginning and is the reference for all takeover attempts
+  public mood: string            = '';  // A string representation of floating liklihood value
+  public name: string            = '';  // Static - doesn't change
+  public owner: number           = 0;   // Changes depending on the game progress
+  public points: number          = 0;   // Static - doesn't change
+  public proGovernment: number   = 0;   // Only used at the starting summary
+  public streetDefection: number = 0;   // The number of streets that might defect on this building's news 
 
   /**
    * Create a building object with the basic details
@@ -22,40 +24,18 @@ export class BuildingService {
    * @param proGovernment 
    * @param points 
    */
-  constructor(@Inject(String) name:string, @Inject(Number) leaning:number, @Inject(Number) proGovernment: number, @Inject(Number) points: number) {
+  constructor(@Inject(String) name:string, @Inject(Number) leaning:number, @Inject(Number) proGovernment: number, @Inject(Number) points: number, streetDefection: number, action: string = '') {
     
-    this.grid          = {'x': 0, 'y': 0}
-    this.image         = name.toLowerCase().replace(' ', '_') + '.png'
-    this.leaning       = leaning;
-    this.name          = name;
-    this.owner         = 0
-    this.points        = points
-    this.proGovernment = proGovernment;
-    
-  }
+    this.action          = action;
+    this.grid            = {'x': 0, 'y': 0};
+    this.image           = name.toLowerCase().replace(' ', '_') + '.png';
+    this.leaning         = leaning;
+    this.name            = name;
+    this.owner           = 0;
+    this.points          = points;
+    this.proGovernment   = proGovernment;
+    this.streetDefection = streetDefection;
 
-  /**
-   * This is a special function, called at the start when the buildings are initalised.
-   * 
-   * @param userType 
-   * @param userPopularity 
-   * @param govtType 
-   * @param govtPopularity 
-   * 
-   * @returns void
-   */
-  setInitialValues(user_type: number, user_popularity: number, govt_type: number, govt_popularity: number): void{
-    let score: number     = 3 - Math.abs(this.leaning - govt_type) + Math.abs(this.leaning - user_type) + this.proGovernment
-    let liklihood: number = Math.floor(score + govt_popularity - user_popularity)
-
-    if (liklihood < 1)
-      liklihood = 1
-    if (liklihood > 5)
-      liklihood = 5
-    
-    // NOTE: this is the ONLY place that this value will be set
-    this.liklihood = liklihood
-    this.mood      = this.getMoodFromLiklihood(liklihood)
   }
 
   /**
@@ -73,28 +53,78 @@ export class BuildingService {
     var liklihood:number = Math.floor(this.liklihood + (square_mood / 2) + govt_popularity - user_popularity);
 
     if (liklihood > 5)
-      liklihood = 5
+      liklihood = 5;
     if (liklihood < 1)
-      liklihood = 1
+      liklihood = 1;
 
-    this.mood = this.getMoodFromLiklihood(liklihood)
+    this.mood = this.getMoodFromLiklihood(liklihood);
     
-    return liklihood
+    return liklihood;
+  }
+
+  getAction(): string {
+
+    var owner: string = '';
+    var action: string = '';
+
+    if (this.owner != 0){
+      if (this.owner == -1){
+        owner = 'rebel';
+      } else if (this.owner == 1){
+        owner = 'government';
+      }
+
+      action = this.action.replace('[x]', owner)
+
+    } else if (this.name.toLowerCase() == 'cathedral'){
+      action = 'The cathedral makes a plea for an end to hostilities';
+    }
+
+    return action;
+  }
+
+  getConsequence(): string {
+
+    var owner: string = '';
+    var consequence: string = '';
+
+    if (this.streetDefection > 0){
+      consequence = 'some [x] forces may switch sides';
+    } else {
+      consequence = '[x] popularity falls';
+    }
+
+    if (this.owner != 0){
+      if (this.owner == -1){
+        owner = 'government';
+      } else if (this.owner == 1){
+        owner = 'rebel';
+      }
+
+      consequence = consequence.replace('[x]', owner)
+    } else if (this.name.toLowerCase() == 'cathedral'){
+      consequence = 'the cathedral makes a plea for an end to hostilities';
+    }
+
+    console.log ('returning consequence:', consequence)
+    return consequence.charAt(0).toUpperCase() + consequence.slice(1);
   }
 
   /**
-   * Store the provided coordinates as the grid location for this building.
-   * This is called when the grid is initially set up at the start of the game.
+   * Get a readable name of this building.
    * 
-   * @param x 
-   * @param y 
-   * 
-   * @returns void
+   * @returns string
    */
-  setGridCoords(x:number, y:number): void{
-    this.grid = {'x': x, 'y': y};
-  }
+  getName(): string{
 
+    var name: string = 'the ' + this.name;
+
+    if (this.name.toLowerCase() == 'xerxes palace'){
+      name = this.name;
+    }
+
+    return name;
+  }
   /**
    * Based on the liklihood number, convert this into a readable string
    * 
@@ -113,5 +143,42 @@ export class BuildingService {
     moods[5] = 'will not';
 
     return moods[liklihood];
+  }
+
+  /**
+   * Store the provided coordinates as the grid location for this building.
+   * This is called when the grid is initially set up at the start of the game.
+   * 
+   * @param x 
+   * @param y 
+   * 
+   * @returns void
+   */
+  setGridCoords(x:number, y:number): void{
+    this.grid = {'x': x, 'y': y};
+  }
+
+  /**
+   * This is a special function, called at the start when the buildings are initalised.
+   * 
+   * @param userType 
+   * @param userPopularity 
+   * @param govtType 
+   * @param govtPopularity 
+   * 
+   * @returns void
+   */
+  setInitialValues(user_type: number, user_popularity: number, govt_type: number, govt_popularity: number): void{
+    let score: number     = 3 - Math.abs(this.leaning - govt_type) + Math.abs(this.leaning - user_type) + this.proGovernment;
+    let liklihood: number = Math.floor(score + govt_popularity - user_popularity);
+
+    if (liklihood < 1)
+      liklihood = 1;
+    if (liklihood > 5)
+      liklihood = 5;
+    
+    // NOTE: this is the ONLY place that this value will be set
+    this.liklihood = liklihood;
+    this.mood      = this.getMoodFromLiklihood(liklihood);
   }
 }
