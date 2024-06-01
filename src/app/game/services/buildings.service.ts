b873adfe0b8dc81d4787896b2041b53af00b5c9b
Geoff Munn <geoff@wxoutside.tools>
2024-06-01 17:24:58 +1200

    Action now passed at building instantiation

--- src/app/game/services/buildings.service.ts
+++ src/app/game/services/buildings.service.ts
@@ -10,21 +10,21 @@ export class BuildingsService {
 
   constructor() {
     this.buildings = [
-      new BuildingService('Government building', 3, 2, 3),
-      new BuildingService('Radio station', 3, 0, 3),
-      new BuildingService('Newspaper office', 3, 0, 2),
-      new BuildingService('Police station', 4, 2, 5),
-      new BuildingService('Military HQ', 5, 1, 8),
-      new BuildingService('Trades Union', 1, 0, 5),
-      new BuildingService('Xerxes Palace', 3, 6, 9),
-      new BuildingService('University', 1,-2, 4),
-      new BuildingService('Airport', 4, 1, 3),
-      new BuildingService('Hospital', 3, 0, 2),
-      new BuildingService('Bank', 3, 3, 1),
-      new BuildingService('Shopping centre', 4, 2, 1),
-      new BuildingService('Cathedral', 3, -1, 2),
-      new BuildingService('Factory', 2, 1, 3),
-      new BuildingService('Jail', 3, -2, 1)
+      new BuildingService('Government building', 3, 2, 3, 0),
+      new BuildingService('Radio station', 3, 0, 3, 20, 'The radio station makes a pro-[x] broadcast'),
+      new BuildingService('Newspaper office', 3, 0, 2, 12, 'The newspaper office releases a pro-[x] newsflash'),
+      new BuildingService('Police station', 4, 2, 5, 0),
+      new BuildingService('Military HQ', 5, 1, 8, 0),
+      new BuildingService('Trades union', 1, 0, 5, 0),
+      new BuildingService('Xerxes palace', 3, 6, 9, 12, 'Xerxes palace receives [x] reinforcements'),
+      new BuildingService('University', 1,-2, 4, 0),
+      new BuildingService('Airport', 4, 1, 3, 0, 'The airport flies supplies in to the [x]', ),
+      new BuildingService('Hospital', 3, 0, 2, 0, 'The hospital gives help to the [x] forces'),
+      new BuildingService('Bank', 3, 3, 1, 0, 'The bank freezes [x] assets'),
+      new BuildingService('Shopping centre', 4, 2, 1, 0),
+      new BuildingService('Cathedral', 3, -1, 2, 10, 'The cathedral preaches a pro-[x] sermon'),
+      new BuildingService('Factory', 2, 1, 3, 0),
+      new BuildingService('Jail', 3, -2, 1, 0)
     ]
   }
 
