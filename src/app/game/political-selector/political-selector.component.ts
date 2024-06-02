4bf84c5375cc07fc78343487ff348f5babd45e6b
Geoff Munn <geoff@wxoutside.tools>
2024-06-03 11:54:18 +1200

    Political selector refactored and documented

--- src/app/game/political-selector/political-selector.component.ts
+++ src/app/game/political-selector/political-selector.component.ts
@@ -9,43 +9,70 @@ import {AfterViewInit, Component, Input, Output, EventEmitter, ElementRef, Rende
 })
 
 export class PoliticalSelection implements AfterViewInit {
-  //politicalSelection!: string;
-  govtTypes: string[] = ['Communist', 'Socialist', 'Liberal', 'Rightwing', 'Fascist'];
+  govt_types: string[] = ['Communist', 'Socialist', 'Liberal', 'Rightwing', 'Fascist'];
 
+  // The heading of this political selection group
   @Input() title: string = '';
-
+  // Used to differentiate between rebels and govt
   @Input() group: string = ''
-
+  // Emits the form change back to the parent component
   @Output() change: EventEmitter<string> = new EventEmitter<string>();
 
-  selectGovtType($event: any) {
+  /**
+   * Change the post based on the government type
+   * 
+   * @param parent_el
+   * @param govt_type
+   *  
+   * @returns boolean
+   */
+  changeImage(parent_el: HTMLElement, govt_type: string): boolean {
+    var img: HTMLIFrameElement|null = parent_el.querySelector('img.politicalPoster');
+    img!.setAttribute('src', '/assets/posters/' + govt_type + '_poster.png');
 
-    console.log('user has selected:', $event.target.value)
+    return true;
+  }
 
-    this.change.emit($event.target.value);
-
+  /**
+   * Triggered by the 'change' event on the political selection section
+   * 
+   * @param $event 
+   * 
+   * @returns void
+   */
+  selectGovtType($event: any): void {
     // Change the image
-    var img = $event.target.parentElement.parentElement.parentElement.parentElement.querySelector('img.politicalPoster');
-    img.setAttribute('src', '/assets/posters/' + $event.target.value.toLowerCase() + '_poster.png')
+    var cur_el: HTMLElement|null = $event.target;
+
+    while (!cur_el!.classList.contains('politicalSelectorContainer'))
+      cur_el = cur_el!.parentElement;
+
+    this.changeImage(cur_el!, $event.target.value.toLowerCase());
   }
 
   ngAfterViewInit(){
 
-    console.log(this.group)
+    // Default to Communist for the rebels
+    var img_parent:HTMLDivElement
 
     if (this.group == 'rebelsType'){
-      var firstRebel = this.el.nativeElement.querySelector("input.political-selector-radio-button[value='Communist']");
-      console.log (firstRebel)
-      firstRebel.checked = true
-      this.change.emit(firstRebel.value);
+      var first_rebel: HTMLInputElement = this.el.nativeElement.querySelector("input.political-selector-radio-button[value='Communist']");
+      first_rebel.checked = true;
+      this.change.emit(first_rebel.value);
+
+      
+      img_parent = this.el.nativeElement.querySelector("div.posterContainer.rebelsType"); 
+      this.changeImage(img_parent, first_rebel.value.toLowerCase())
     }
 
+    // Default to Fascist for the government
     if (this.group == 'govtType'){
-      var lastGovt = this.el.nativeElement.querySelector("input.political-selector-radio-button[value='Fascist']");
-      console.log (lastGovt.value)
-      lastGovt.checked = true
+      var last_govt: HTMLInputElement = this.el.nativeElement.querySelector("input.political-selector-radio-button[value='Fascist']");
+      last_govt.checked = true;
+      this.change.emit(last_govt.value);
 
-      this.change.emit(lastGovt.value);
+      img_parent = this.el.nativeElement.querySelector("div.posterContainer.govtType");       
+      this.changeImage(img_parent, last_govt.value.toLowerCase())
     }
   }
 
